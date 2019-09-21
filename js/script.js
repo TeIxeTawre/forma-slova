9e87b8a02635b5d56e4c7eb3c4c3e93ed7f75b3b
TeIxe <teixetawrex@gmail.com>
2019-09-21 12:27:50 +0300

    add menu

--- js/script.js
+++ js/script.js
@@ -7,4 +7,15 @@ jQuery(document).ready(function() {
 		items: 1,
 		nav: false
 	});
+
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent))
+		$('body').addClass('active-phone');
+	$(window).resize(function() {
+		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+		    $('body').addClass('active-phone');
+		} else {
+			$('body').removeClass('active-phone');
+		}
+	});
+	
 });
