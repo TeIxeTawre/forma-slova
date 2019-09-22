b866885dd5e24d1ee3de1ab00a14d88cd3749037
TeIxe <teixetawrex@gmail.com>
2019-09-23 02:21:49 +0300

    add last section

--- js/script.js
+++ js/script.js
@@ -5,17 +5,8 @@ jQuery(document).ready(function() {
 
 	$('.header-two-slider').owlCarousel({
 		items: 1,
-		nav: false
-	});
-
-	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent))
-		$('body').addClass('active-phone');
-	$(window).resize(function() {
-		if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-		    $('body').addClass('active-phone');
-		} else {
-			$('body').removeClass('active-phone');
-		}
+		nav: false,
+		loop: true
 	});
 	
 });
