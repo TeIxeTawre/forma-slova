999bd80256ee14e820075af2f47d1738a453aa85
TeIxe <teixetawrex@gmail.com>
2019-09-24 23:22:44 +0300

    edit all maket

--- js/script.js
+++ js/script.js
@@ -9,4 +9,29 @@ jQuery(document).ready(function() {
 		loop: true
 	});
 	
+	if($('#card-second').children('.card').length == 3) {
+		$('#card-second').addClass('threeGrid');
+	}
+
+	if($('#card-first').children('.card') == 3) {
+		$('#card-first').addClass('threeGrid');
+	}
+
+	$('input').focus(function() {
+		$(this).closest('.label-pair').children('label').css('display', 'none');
+	});
+
+	$('input').focusout(function() {
+		if ($(this).val() == '')
+			$(this).closest('.label-pair').children('label').css('display', 'block');
+	});	
+	
+	$('textarea').focus(function() {
+		$(this).closest('.label-pair').children('label').css('display', 'none');
+	});
+	$('textarea').focusout(function() {
+		if ($(this).val() == '')
+			$(this).closest('.label-pair').children('label').css('display', 'block');
+	});
+
 });
